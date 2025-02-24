aa8594f77ec0471c9df5db1962c95eec3f6688bb
Jimmy Huang <jimmy.huang@duragility.com>
2025-02-24 00:41:00 -0800

    added activated apis

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
   gcp_billing_account_id         = data.tfe_outputs.gcp_bootstrap.values.gcp_billing_account_id
+  gcp_org_id                     = data.tfe_outputs.gcp_bootstrap.values.gcp_org_id
   gcp_folder_id                  = data.tfe_outputs.gcp_bootstrap.values.gcp_folder_id
   terraform_admin_gcp_project_id = data.tfe_outputs.gcp_bootstrap.values.terraform_admin_gcp_project_id
 
--- project.tf
+++ project.tf
@@ -1,14 +1,21 @@
-module "service_project" {
-  source  = "terraform-google-modules/project-factory/google"
+module "service-project" {
+  source  = "terraform-google-modules/project-factory/google//modules/svpc_service_project"
   version = "~> 18.0"
 
-  name                 = "service"
-  random_project_id    = true
-  folder_id            = local.gcp_folder_id
-  billing_account      = local.gcp_billing_account_id
-  
-  svpc_host_project_id = local.shared_host_vpc_project_id
+  name              = "service"
+  random_project_id = false
 
+  org_id          = local.gcp_org_id
+  folder_id       = local.gcp_folder_id
+  billing_account = local.gcp_billing_account_id
+
+  shared_vpc         = local.shared_host_vpc_project_id
   shared_vpc_subnets = data.google_compute_network.shared_host_vpc.subnetworks_self_links
-  grant_network_role = true
+
+  activate_apis = [
+    "compute.googleapis.com",
+  ]
+
+  disable_services_on_destroy = false
+  deletion_policy             = "DELETE"
 }
